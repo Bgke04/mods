bd637708c0bf21de9f82950448e929f98f4941a6
Bgke04 <161800431+Bgke04@users.noreply.github.com>
2025-02-13 23:54:40 +0700

    jjju
    jj

---
+++ index.php
@@ -0,0 +1,105 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hacked By Bgke04 Dev</title>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Bgke04 Dev" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <meta property="og:title" content="Hacked By Bgke04 Dev - Indonesian Defacer" />
+    <meta name="keywords" content="Bgke04 Dev - Indonesian Defacer, Hacked By Bgke04 Dev, hacked by Bgke04 Dev, Bgke04 Dev, Hacked, Bgke04 Dev, Bgke04, touched by, hacked by, touched by bgke04 dev" />
+    <meta property="og:image" content="https://cdn.prinsh.com/data-1/images/NathanPrinsley-IndonesianAnonymous.jpg" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Haxor Uploader" />
+    <link rel="shortcut icon" type="image/x-icon" href="https://cdn.prinsh.com/data-1/images/NathanPrinsley-IndonesianAnonymous.jpg" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.prinsh.com/NathanPrinsley-textstyle/nprinsh-stext.css" />
+    <style>
+    .thanks {
+      p
+      display: block;
+            position: fixed;
+            width: 100%;
+            top: 43%;
+            font-family: Tahoma;
+            height: 20px;
+            color: white;
+            left: 0%;
+            padding: 5px;
+            font-size: 2vh;
+            text-decoration: bold;
+    }
+        body {
+            background: black;
+            font-family: Calibri;
+            margin-top: 35px;
+        }
+        h1,
+        h2 {
+            margin-top: 0.3em;
+            margin-bottom: 0.3em;
+            font-size: 2vh;
+        }
+        h1.nprinsleyy {
+            color: #de0707;
+            font-size: 2.5vh;
+        }
+        h2 {
+            color: #dbd9d9;
+            font-size: 2vh;
+        }
+        p.message_prinsley {
+            color: #dbd9d9;
+            margin-top: 0.25em;
+            margin-bottom: 0.25em;
+            font-size: 2vh;
+            font-weight: unset;
+        }
+        .hubungi_prinsh {
+            color: #00eb00;
+            text-decoration: block;
+        }
+        .woy {
+          height: 18vh;
+        }
+        .hubungi_prinsh:hover {
+            color: red;
+        }
+        .othermes_nprinsh {
+            color: #00eb00;
+            font-size: 2vh;
+        }
+        marquee.foonathanPrinsley {
+            display: block;
+            position: fixed;
+            width: 100%;
+            top: 48%;
+            font-family: Tahoma;
+            color: white;
+            left: 0%;
+  
+          font-size: 2vh;
+        }
+    </style>
+</head>
+<body onclick="playAudio()">
+    <center/>
+    <img src="https://cdn.prinsh.com/data-1/images/NathanPrinsley-IndonesianAnonymous.jpg" class="woy" />
+    <h1 class="nprinsleyy nprinsley-text-glitchan" style="font-size:3.5vh;">Hacked By Bgke04 Dev</h1>
+    <h2 style="font-size:3vh;" class="nprinsley-text-glitchan">Bgke04 Wass Here!</h2>
+    <p class="message_prinsley nathan-prinsley_none"></p>
+    <p style="font-size:2vh;" class="nathan-prinsley_none"></p>
+    <p class="othermes_nprinsh nathan-prinsley_none">bagi kami dunia nyata itu hanya penuh penderitaan<br>tapi disini kami dapat mencari kebahagian</p>
+    <p class="othermes_nprinsh nathan-prinsley_none"> t.me/Bgke04Dev </p>
+    <marquee class="foonathanPrinsley"></marquee>
+    <script src="https://cdn.prinsh.com/NathanPrinsley-effect/efek-salju.js" type="text/javascript"></script>
+    <script>
+      function playAudio() {
+        const audio = new Audio('https://github.com/Bgke04/mods/raw/refs/heads/main/lagu.mp3');
+        audio.loop = true;
+        audio.play().catch(e => console.log('Autoplay gagal:', e));
+        document.body.onclick = null; 
+      }
+    </script>
+</body>
+</html>
